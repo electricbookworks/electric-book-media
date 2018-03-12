4370e4c365d8ae50c36b496887502c210b70e527
Arthur Attwell <arthur@arthurattwell.com>
2018-03-12 16:21:11 +0200

    Make more readable

--- run-linux.sh
+++ run-linux.sh
@@ -2,6 +2,8 @@
 # You can use that URL as a remote-media development path.
 # (Unlike the command we use on Windows, this can be killed with Ctrl+C on unix.)
 
+echo "-------------------------------------------"
 echo "Starting webserver at http://localhost:5000"
 echo "Press Ctrl+C to stop"
+echo "-------------------------------------------"
 ruby -rwebrick -e'WEBrick::HTTPServer.new(:Port => 5000, :DocumentRoot => Dir.pwd).start'
