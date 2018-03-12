f84add5ac4a565f232f9ae78893f9dd961651af8
Arthur Attwell <arthur@arthurattwell.com>
2018-03-12 16:21:59 +0200

    Add notes

--- run-windows.bat
+++ run-windows.bat
@@ -1,5 +1,16 @@
+:: Don't show these commands to the user
+@echo off
+:: Keep variables local, and expand at execution time not parse time
+setlocal enabledelayedexpansion
+:: Set the title of the window
+title Electric Book Media webserver
+
 :: This will serve external media as a website at http://localhost:5000
 :: You can use that URL as a remote-media development path.
 :: (See http://www.benjaminoakes.com/2013/09/13/ruby-simple-http-server-minimalist-rake/)
 
+echo -------------------------------------------
+echo Starting webserver at http://localhost:5000
+echo Press Ctrl+C to stop
+echo -------------------------------------------
 ruby -run -e httpd . -p 5000
